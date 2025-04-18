02605a98ef592a0ff478e5322942138b054f083e
Animesh gupta <animesh.gupta.0207@gmail.com>
2025-04-18 21:55:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// calculator program by animesh Gupta 
+const display = document.getElementById("display");
+
+function appendTodisplay(input){
+display.value += input;
+}
+function clearDisplay(){
+display.value= "";
+}
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch{
+        display.value="ERROR";
+    }
+}
